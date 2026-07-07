bcd53e6ed6d5e3d809f65758f584e55ba8b99100
seichris <cerujurasa@gmail.com>
2026-07-07 12:21:41 +0800

    Use production offline map domain

--- ios-app/BikeComputer/BikeComputer/Models/OfflineMapServiceConfig.swift
+++ ios-app/BikeComputer/BikeComputer/Models/OfflineMapServiceConfig.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 enum OfflineMapServiceConfig {
-    static let productionServerURLString = "http://rhi0maej6bwo33hn0im6h4lf.178.18.245.246.sslip.io"
+    static let productionServerURLString = "https://maps.8o.vc"
     static let apiTokenInfoPlistKey = "OfflineMapAPIToken"
 
     static var apiToken: String {
